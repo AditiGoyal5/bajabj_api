c815a0820ee0175af5610af815a355f6b97309f4
CodeSandbox <support@codesandbox.io>
2024-03-18 07:12:38 +0000

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,31 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+router.post("/bfhl", function (req, res, next) {
+  const { array } = req.body;
 
+  const numArray = [];
+  const stringArray = [];
+
+  array.forEach((item) => {
+    if (typeof item === "number") {
+      numArray.push(item);
+    } else if (typeof item === "string") {
+      stringArray.push(item.toUpperCase());
+    }
+  });
+
+  const oddArray = numArray.filter((num) => num % 2 !== 0);
+  const evenArray = numArray.filter((num) => num % 2 === 0);
+  const response = {
+    is_success: true,
+    user_id: "aditigoyal_05082003",
+    email: "aditi1617.be21@chitkara.edu.in",
+    roll_number: "2110991617",
+    odd_numbers: oddArray,
+    even_numbers: evenArray,
+    alphabets: stringArray,
+  };
+  res.json(response);
+});
 module.exports = router;
